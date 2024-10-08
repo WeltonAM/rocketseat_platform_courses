0836d6680dc40102f089ea56299bb1648e713288
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-08 15:12:18 -0300

    Dia 2 - Frontend livewire components

--- nwl_freelance_platform/routes/web.php
+++ nwl_freelance_platform/routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\WelcomeController;
 
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::get('/', [ProjectsController::class, 'index'])->name('projects.index');
+Route::get('/project/{project}', [ProjectsController::class, 'show'])->name('projects.show');
